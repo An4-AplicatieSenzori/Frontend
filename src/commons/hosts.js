a5ae5657704ecb622da7ebfd90e4db2044b95536
OrsanTudor <100350307+OrsanTudor@users.noreply.github.com>
2023-01-17 18:21:08 +0200

    Commit frontend final

--- src/commons/hosts.js
+++ src/commons/hosts.js
@@ -5,5 +5,5 @@ export const HOST = {
     backend_api: 'http://localhost:8080', //API principal, de backend;
 
     //CELE DE AZURE!!!
-    //backend_api: 'http://20.113.109.129:8080',
+    //backend_api: 'http://20.52.193.76:8080', //Done :)
 };
